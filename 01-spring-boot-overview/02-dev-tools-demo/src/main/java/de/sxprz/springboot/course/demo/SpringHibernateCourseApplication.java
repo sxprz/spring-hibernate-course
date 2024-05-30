86483fa5b50bc95ec014480f21d92a82f2f55e23
sxprz <derian.pljakic@gmail.com>
2024-05-30 22:06:42 +0200

    01-spring-boot-overview: Introduce dev tools

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/de/sxprz/springboot/course/demo/SpringHibernateCourseApplication.java
@@ -0,0 +1,13 @@
+package de.sxprz.springboot.course.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateCourseApplication.class, args);
+	}
+
+}
