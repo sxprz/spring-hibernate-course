a8bbf840ed4e1a9aa87d589d247878e5a6e59a9b
sxprz <derian.pljakic@gmail.com>
2024-06-05 21:38:39 +0200

    01-spring-boot-overview: Add security to standard endpoints

---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/test/java/de/sxprz/springboot/course/demo/SpringHibernateCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sxprz.springboot.course.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
