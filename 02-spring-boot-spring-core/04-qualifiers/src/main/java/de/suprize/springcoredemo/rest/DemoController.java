88be868a594c32dad1ab9990216e86803ace0557
sxprz <derian.pljakic@gmail.com>
2024-06-10 22:14:31 +0200

    02-spring-boot-spring-core: Use qualifiers for injection to remove ambiguity

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/de/suprize/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package de.suprize.springcoredemo.rest;
+
+import de.suprize.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach coach;
+
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") final Coach coach) {
+        this.coach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+
+}
