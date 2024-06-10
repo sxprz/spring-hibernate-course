e7b54b0ebf44cea5506d65fcfaa389597e60b46a
sxprz <derian.pljakic@gmail.com>
2024-06-10 21:54:04 +0200

    02-spring-boot-spring-core: Organise project into more modules / sub-packages

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/de/suprize/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package de.suprize.springcoredemo.rest;
+
+import de.suprize.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach coach;
+
+    // define a constructor for dependecy injection
+    // `@Autowired` annotation optional when only using one constructor
+    @Autowired
+    public DemoController(Coach coach) {
+        this.coach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+
+}
