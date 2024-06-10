794e6a58a86468a711df26576e6dcf4d6f1d4f49
sxprz <derian.pljakic@gmail.com>
2024-06-10 22:28:50 +0200

    02-spring-boot-spring-core: Use the `@Lazy` annotation for lazy initialization for single beans

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/de/suprize/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,19 @@
+package de.suprize.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + this.getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+}
