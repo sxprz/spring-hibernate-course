0da80b3ecdb0f83313778f9a4abd291c04d6def4
sxprz <derian.pljakic@gmail.com>
2024-06-14 22:24:52 +0200

    03-spring-boot-hibernate-jpa-crud: Create `StudentDAO` + implentation and use `EntityManager`

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/de/suprize/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package de.suprize.cruddemo.dao;
+
+import de.suprize.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+
+}
---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/de/suprize/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,24 @@
+package de.suprize.cruddemo.dao;
+
+import de.suprize.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        this.entityManager.persist(student);
+    }
+}
