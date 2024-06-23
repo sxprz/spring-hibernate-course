62e57d0310fae451cd4e544b594f23590637ee5f
sxprz <derian.pljakic@gmail.com>
2024-06-23 18:22:07 +0200

    04-spring-boot-rest-crud: Impl `/students/{studentId}` endpoint

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/de/suprize/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/de/suprize/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package de.suprize.demo.rest;
 import de.suprize.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class StudentRestController {
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 
     @GetMapping("/students")
