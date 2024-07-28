817e422c25a61652016ed5a1203b47f4f2b3d853
sxprz <derian.pljakic@gmail.com>
2024-07-28 10:51:29 +0200

    04-spring-boot-rest-crud: Setup new project

---
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/test/java/de/suprize/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.suprize.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
