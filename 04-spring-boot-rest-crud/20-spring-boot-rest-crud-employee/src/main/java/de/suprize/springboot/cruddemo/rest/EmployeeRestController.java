84494b1ac5b06e94622fdb711b4ff1c42af55878
sxprz <derian.pljakic@gmail.com>
2024-07-28 11:15:45 +0200

    04-spring-boot-rest-crud: Impl rest controller for DAO

---
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/de/suprize/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package de.suprize.springboot.cruddemo.rest;
+
+import de.suprize.springboot.cruddemo.dao.EmployeeDAO;
+import de.suprize.springboot.cruddemo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return this.employeeDAO.findAll();
+    }
+
+}
