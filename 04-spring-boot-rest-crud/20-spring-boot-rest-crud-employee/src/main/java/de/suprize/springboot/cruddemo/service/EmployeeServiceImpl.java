8faf146912cdf569853d3969d0161b9e7d9c3548
sxprz <derian.pljakic@gmail.com>
2024-07-31 13:01:43 +0200

    04-spring-boot-rest-crud: Use DAO in service

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/de/suprize/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/de/suprize/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import de.suprize.springboot.cruddemo.dao.EmployeeDAO;
 import de.suprize.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,7 +20,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return this.employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return this.employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return this.employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+        this.employeeDAO.deleteById(id);
     }
 
 }
