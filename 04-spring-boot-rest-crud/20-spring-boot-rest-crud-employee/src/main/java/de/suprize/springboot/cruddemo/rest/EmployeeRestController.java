e7cdd80e7534053ba4c116915e1cbe7e38d0cba3
sxprz <derian.pljakic@gmail.com>
2024-07-31 13:06:48 +0200

    04-spring-boot-rest-crud: Impl endpoint for getting single employees

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/de/suprize/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/de/suprize/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package de.suprize.springboot.cruddemo.rest;
 import de.suprize.springboot.cruddemo.entity.Employee;
 import de.suprize.springboot.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,16 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return this.employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = this.employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
     }
 
 }
