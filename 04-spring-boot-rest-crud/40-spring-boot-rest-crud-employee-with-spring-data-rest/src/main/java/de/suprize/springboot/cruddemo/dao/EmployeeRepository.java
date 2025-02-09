43acbda217a2849162e51c91b35d1cd1b63ab9f4
sxprz <derian.pljakic@gmail.com>
2025-02-09 19:30:16 +0100

    04-spring-boot-rest-crud: Customize REST API path

--- 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/de/suprize/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/de/suprize/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package de.suprize.springboot.cruddemo.dao;
 
 import de.suprize.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
