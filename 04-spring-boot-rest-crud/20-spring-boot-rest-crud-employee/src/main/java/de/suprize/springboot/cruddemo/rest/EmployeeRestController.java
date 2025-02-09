5e8e7f71c30b74ee4b89a1ec308d7ab485fd5173
sxprz <derian.pljakic@gmail.com>
2025-02-09 11:59:03 +0100

    04-spring-boot-rest-crud: Impl endpoint for deleting a single employee and refactor code a bit

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/de/suprize/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/de/suprize/springboot/cruddemo/rest/EmployeeRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     public EmployeeRestController(EmployeeService employeeService) {
         this.employeeService = employeeService;
@@ -18,14 +18,14 @@ public class EmployeeRestController {
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return this.employeeService.findAll();
+        return employeeService.findAll();
     }
 
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
-        Employee employee = this.employeeService.findById(employeeId);
+        Employee employee = employeeService.findById(employeeId);
         if (employee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
+            throw new IllegalArgumentException("Employee id not found - " + employeeId);
         }
 
         return employee;
@@ -34,20 +34,27 @@ public class EmployeeRestController {
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee employee) {
         // Because of the functionality of entityManager.merge(), we set the id to 0
-        // if id == 0, it inserts, else it updates an existing employee with the given id
+        // if id == 0, it inserts, otherwise it updates an existing employee with the given id
         employee.setId(0);
 
-        return this.employeeService.save(employee);
+        return employeeService.save(employee);
     }
 
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee employee) {
-        return this.employeeService.save(employee);
+        return employeeService.save(employee);
     }
 
     @DeleteMapping("/employees/{employeeId}")
-    public void deleteEmployee(@PathVariable int employeeId) {
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
 
+        if (employee == null) {
+            throw new IllegalArgumentException("Employee not found with id " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+        return "Deleted employee with id " + employeeId + " successfully";
     }
 
 }
